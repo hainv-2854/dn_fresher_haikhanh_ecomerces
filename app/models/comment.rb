44c3069f5f9e7eddb69308b7c48a7a101d68768a
nguyen.van.hai <SUN-ASTERISK\nguyen.van.hai@b122854-pc.sun-asterisk.com>
2022-01-04 16:12:10 +0700

    setup sun-ci

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
   belongs_to :parent, class_name: Comment.name, optional: true
-  has_many :childrens, class_name: Comment.name, foreign_key: :parent_id, dependent: :destroy
+  has_many :childrens, class_name: Comment.name, foreign_key: :parent_id,
+    dependent: :destroy
 end
