ae66a6a193f9049473341f198bbb915aece90a7a
khanhpt-2853 <pham.tan.khanh@sun-asterisk.com>
2022-01-06 17:08:23 +0700

    User: Detail product

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Example User",
+  email: "user@gmail.com",
+  password: "user123",
+  password_confirmation: "user123",
+  role: 1,
+  activated: true
+)
+10.times do |n|
+  name = Faker::Name.name
+  email = "example#{n+1}@project.org"
+  password = "password"
+  User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    activated: true
+  )
+end
+
+category = ["Dell", "HP", "Lenovo", "Macbook"]
+
+3.times do |n|
+  Category.create(name: category[n])
+end
+
+50.times do |n|
+  category_id = 1
+  category_id = 2 if n > 25
+  product = Product.create(
+    name: Faker::Name.first_name,
+    category_id: category_id,
+    price: 120000,
+    quantity: 10,
+    image: "products/digital_20.jpg",
+    description: Faker::Lorem.sentence,
+  )
+end
