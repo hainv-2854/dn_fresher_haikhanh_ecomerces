9ad0686d2120c5a44c428974300df5c7164aac01
khanhpt-2853 <pham.tan.khanh@sun-asterisk.com>
2022-01-06 17:08:23 +0700

    User Login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user&.authenticate params[:session][:password]
+      log_in user
+      redirect_to root_url
+    else
+      @email = params[:session][:email]
+      flash.now[:danger] = t ".invalid_email_password"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_secure_password
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
 end
